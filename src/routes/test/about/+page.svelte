33c411b46574577074dd93b3a9695580b7e72631
shibest <eerkingtheawesome@gmail.com>
2024-09-07 15:30:48 -0700

    test stuff
    need to move some stuff to layout.svelte soon

---
+++ src/routes/test/about/+page.svelte
@@ -0,0 +1,30 @@
+<div class="logo">
+    <h1>Placeholder</h1>
+</div>
+<div class="navbar">
+    <a href="/test">Home</a>
+    <a href="/test/communicate">Communicate</a>
+    <a class="selected" href="/test/about">About</a>
+</div>
+
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+
+    .logo{
+        text-align: center;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+    }
+    .navbar{
+        text-align: center;
+        
+    }
+    .selected{
+        color: black;
+        background-color: white;
+    }
+</style>
