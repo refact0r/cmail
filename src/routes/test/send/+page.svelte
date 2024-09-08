63d04e8db9574d04fb61c80d3bafcf25872ac2f9
shibest <eerkingtheawesome@gmail.com>
2024-09-08 10:42:31 -0700

    work on send page

--- src/routes/test/send/+page.svelte
+++ src/routes/test/send/+page.svelte
@@ -1,11 +1,28 @@
 <script>
     import { profile } from '$lib/stores/profile.js';
+    import { dist, scaleDiam } from '$lib/js/planets.js';
+    import { formatAU, formatSecs } from '$lib/js/utils.js';
+    import * as constants from '$lib/js/constants.js';
+	import { afterUpdate } from 'svelte';
+
+    export let data;
 
     let destination = 'earth';
-    let distance;
+    let distance_km;
+    let distance_au;
     let delay;
 
-    
+    distance_km = dist(data.planets[$profile], data.planets[destination]);
+    distance_au = formatAU(distance_km);
+    let delay_sec = (distance_km / constants.c) * 1000;
+    delay = formatSecs(delay_sec);
+
+    function updateVals() {
+        distance_km = dist(data.planets[$profile], data.planets[destination]);
+        distance_au = formatAU(distance_km);
+        let delay_sec = (distance_km / constants.c) * 1000;
+        delay = formatSecs(delay_sec);
+    }
 
     function sendMessage() {
         //posts message and delay to database
@@ -17,66 +34,75 @@
     <div class="form-box">
         <form on:submit|preventDefault={() => sendMessage()}>
             <div class="row">
-                <div class="column">
+                <div class="half">
+                    <svg viewBox="0 0 2000 1000">
+                        <line 
+                            x1 = 300
+                            y1 = 500
+                            x2 = 1700
+                            y2 = 500
+                            stroke="currentColor"
+                            stroke-width="10"
+                            stroke-dasharray='35,30'
+                        />
+                        <text
+                            x=1000
+                            y=400
+                            text-anchor="middle"
+                            fill="white"
+                            font-size=3.5rem
+                        >Calculated Distance: {distance_au} AU</text>
+                        <text
+                            x=1000
+                            y=650
+                            text-anchor="middle"
+                            fill="white"
+                            font-size=3.5rem
+                        >Calculated Delay: {delay}</text>
+
+                        <circle 
+                            cx={300} 
+                            cy={500} 
+                            r={scaleDiam(data.planets[$profile].diameter)*10} 
+                            fill={data.planets[$profile].color}
+                        />
+                        <circle 
+                            cx={1700} 
+                            cy={500} 
+                            r={scaleDiam(data.planets[destination].diameter)*10} 
+                            fill={data.planets[destination].color} 
+                        />
+                        
+                    </svg>
+                </div>
+                <div class="half fill">
                     <div class="row">
                         <div class="group">
                             <label for="profile">Profile: </label>
                             <br>
-                            <select name="profile" id="profile" title="Select planet" bind:value={$profile}>
-                                <option value="mercury" title="Mercury">Mercury</option>
-                                <option value="venus" title="Venus">Venus</option>
-                                <option value="earth" title="Earth">Earth</option>
-                                <option value="moon" title="Moon">Moon</option>
-                                <option value="mars" title="Mars">Mars</option>
-                                <option value="jupiter" title="Jupiter">Jupiter</option>
-                                <option value="saturn" title="Saturn">Saturn</option>
-                                <option value="uranus" title="Uranus">Uranus</option>
-                                <option value="neptune" title="Neptune">Neptune</option>
-                                <option value="pluto" title="Pluto">Pluto</option>
-                                </select>
+                            <select name="profile" id="profile" title="Start" bind:value={$profile} on:change|preventDefault={() => updateVals()}>
+                                {#each Object.keys(data.planets) as planet}
+                                    <option value={planet}>{planet}</option>
+                                {/each}
+                            </select>
                         </div>
                         <div class="group">
                             <label for="destination">Destination</label>
                             <br />
-                            <select name="destination" id="destination" title="Destination" bind:value={destination}>
-                                <option value="mercury" title="Mercury">Mercury</option>
-                                <option value="venus" title="Venus">Venus</option>
-                                <option value="earth" title="Earth">Earth</option>
-                                <option value="moon" title="Moon">Moon</option>
-                                <option value="mars" title="Mars">Mars</option>
-                                <option value="jupiter" title="Jupiter">Jupiter</option>
-                                <option value="saturn" title="Saturn">Saturn</option>
-                                <option value="uranus" title="Uranus">Uranus</option>
-                                <option value="neptune" title="Neptune">Neptune</option>
-                                <option value="pluto" title="Pluto">Pluto</option>
+                            <select name="destination" id="destination" title="Destination" bind:value={destination} on:change|preventDefault={() => updateVals()}>
+                                {#each Object.keys(data.planets) as planet}
+                                    <option value={planet}>{planet}</option>
+                                {/each}
                             </select>
                         </div>
-                    </div>
+                        <input type="submit" value="➤" title="Send">
+                    </div>  
                     <br>
                     <div class="group">
                         <label for="message">Message</label>
                         <br>
                         <textarea name="message" id="message" maxlength="400" required></textarea>
-                    </div>
-                </div>
-                <div class="column"></div><div class="column"></div><div class="column"></div>
-                <div class="column">
-                    <div class="row">
-                        <div class="group">
-                            <label for="distance">Calculated Distance</label>
-                            <br>
-                            <input type="text" id="distance" name="distance" title="Distance" bind:value={distance} readonly> AU    
-                        </div>
-                        <div class="group">
-                            <label for="delay">Calculated Delay</label>
-                            <br>
-                            <input type="text" id="delay" name="delay" title="Delay" bind:value={delay} readonly> seconds
-                        </div>
-                    </div>
-                </div>
-                <div class="column"></div><div class="column"></div><div class="column"></div><div class="column"></div><div class="column"></div><div class="column"></div><div class="column">
-                <div class="column r">
-                    <input type="submit" value="➤" title="Send">
+                    </div>                    
                 </div>
             </div>
         </form>
@@ -94,26 +120,35 @@
     }
     form {
         padding: 4rem;
-        background: var(--bg-3);
+        /*background: var(--bg-3);*/
         border-radius: 1rem;
     }
     .row {
         display: flex;
         gap: 2rem;
     }
-    select{
-		border-radius: .2rem;
-		outline: none;
-		font-family: 'Orbitron', sans-serif;
+    .half {
+        flex: 50%;
+    }
+    select {
+		border: 2px solid var(--bg-2);
+		padding: 0.5rem 0.75rem;
+		border-radius: 1rem;
 		background-color: var(--bg);
-		color: var(--fg);
-		width: 8vw;
-		transition: .3s;
+		font: inherit;
+		color: inherit;
+		transition: 0.1s;
 	}
-	select:hover{
+	select:hover {
+		cursor: pointer;
 		background-color: var(--fg);
 		color: var(--bg);
 	}
+    .fill {
+        background-color: var(--bg-3);
+        padding: 2rem;
+        border-radius: 1rem;
+    }
     textarea{
         width: 100%;
         resize: none;
@@ -135,21 +170,23 @@
         text-decoration: none;
         border-radius: 4rem;
         transition: .3s;
+        height: 10%;
+        margin-top: 1vh;
+        justify-content: flex-end;
+        margin-left: auto;
     }
     input[type="submit"]:hover {
         color: var(--bg);
         background: var(--fg);
         border: var(--bg) .1rem solid;
     }
-    input[type="text"] {
-        border-radius: .2rem;
-		outline: none;
-		font-family: 'Orbitron', sans-serif;
-		background-color: var(--bg);
-		color: var(--fg);
-		width: 8vw;
-		transition: .3s;
-        border: var(--fg) 1px solid;
+    .r {
+        align-items: right;
+        align-self: right;
+        align-content: right;
+        flex: 5%;
     }
-
+    .vh {
+        flex: 25%;
+    }
 </style>
