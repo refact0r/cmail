8ffee1c3e74fd1a74943b8f004d1578424fde412
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-08 08:53:15 -0700

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,19 @@
 </script>
 
 <div class="page">
-	<div class="left"></div>
+	<div class="side">
+		<div class="new box">
+			<h2>New</h2>
+		</div>
+	</div>
 	<div class="system">
 		<SolarSystem {data} />
 	</div>
-	<div class="right"></div>
+	<div class="side">
+		<div class="outgoing box">
+			<h2>Outgoing</h2>
+		</div>
+	</div>
 </div>
 
 <style>
@@ -17,11 +25,21 @@
 		display: flex;
 		margin-top: 0rem;
 	}
-	.left {
+	.side {
 		width: 25%;
+		display: flex;
+		align-items: center;
+		padding: 1rem;
 	}
-	.right {
-		width: 25%;
+	.box {
+		background: var(--bg-2);
+		border-radius: 1rem;
+		border: 2px solid var(--bg-3);
+		padding: 1rem;
+		width: 100%;
+	}
+	.box h2 {
+		margin: 0 0 1rem 0;
 	}
 	.system {
 		width: 50%;
