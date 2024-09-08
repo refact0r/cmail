0b41a6fec00caaf0ad82c6a026f8c6e903cb3be8
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-07 22:18:43 -0700

    some styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,28 +11,27 @@
 </svelte:head>-->
 
 <div class="app">
+	<div class="logo">
+		<h1>Placeholder</h1>
+	</div>
+	<div class="navbar">
+		<a class="selected" href="/test">‎ Home ‎</a>
+		<a href="/test/inbox">‎ Inbox ‎</a>
+		<a href="/test/send">‎ Send ‎</a>
+		<a href="/test/about">‎ About ‎</a>
+	</div>
+	<div class="box">
+		<div class="server-select">
+			<label for="server">Profile: </label>
+			<select name="server" id="server">
+				<option value="earth">Earth</option>
+				<option value="moon">Moon</option>
+				<option value="mars">Mars</option>
+			</select>
+		</div>
+	</div>
 	{#key data.pathname}
-		<div class="main" in:fade={{ duration: 200, delay: 200 }} out:fade={{ duration: 200 }}>
-			<div class="logo">
-				<h1>Placeholder</h1>
-			</div>
-			<div class="navbar">
-				<a class="selected" href="/test">‎ Home ‎</a>
-				<a href="/test/inbox">‎ Inbox ‎</a>
-				<a href="/test/send">‎ Send ‎</a>
-				<a href="/test/about">‎ About ‎</a>
-			</div>
-			<div class="box">
-				<div class="server-select">
-					<label for="server">Profile: </label>
-					<select name="server" id="server">
-						<option value="earth">Earth</option>
-						<option value="moon">Moon</option>
-						<option value="mars">Mars</option>
-					  </select>
-				</div>
-			</div>
-			
+		<div class="main" in:fade={{ duration: 100, delay: 50 }} out:fade={{ duration: 100 }}>
 			<slot />
 		</div>
 	{/key}
@@ -53,25 +52,24 @@
 		padding: 1.5rem 0;
 	}
 
-	.logo{
-        text-align: center;
-    }
-    a {
-        color: white;
-        text-decoration: none;
-    }
-    .navbar{
-        text-align: center;
-        
-    }
-	.selected{
-        color: black;
-        background-color: white;
-    }
-	.server-select{
-        text-align: center;
-    }
-	.box{
+	.logo {
+		text-align: center;
+	}
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	.navbar {
+		text-align: center;
+	}
+	.selected {
+		color: black;
+		background-color: white;
+	}
+	.server-select {
+		text-align: center;
+	}
+	.box {
 		padding-top: 1rem;
 	}
 </style>
