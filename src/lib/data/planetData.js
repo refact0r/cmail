e807cb8db966483068738a9de0f7ed8966e5d863
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-07 22:52:20 -0700

    improve planets

--- src/lib/data/planetData.js
+++ src/lib/data/planetData.js
@@ -1,59 +1,11 @@
 export default [
 	{
-		name: 'Mercury',
-		diameter: 4879,
-		period: 88.0,
-		distance: 57.9,
-		startDegrees: 252.4100389327003,
-		color: '#bfbdbc'
-	},
-	{
-		name: 'Venus',
-		diameter: 12104,
-		period: 224.7,
-		distance: 108.2,
-		startDegrees: 181.79362286556446,
-		color: '#d79a4c'
-	},
-	{
-		name: 'Earth',
-		diameter: 12756,
-		period: 365.2,
-		distance: 149.6,
-		startDegrees: 99.86813733232074,
-		color: '#5e98b2'
-	},
-	{
-		name: 'Mars',
-		diameter: 6792,
-		period: 687.0,
-		distance: 227.9,
-		startDegrees: 359.13427034704614,
-		color: '#f27b5f'
-	},
-	{
-		name: 'Jupiter',
-		diameter: 142984,
-		period: 4332.0,
-		distance: 778.5,
-		startDegrees: 36.24901810964624,
-		color: '#bb9b87'
-	},
-	{
-		name: 'Saturn',
-		diameter: 120536,
-		period: 10747.0,
-		distance: 1432.0,
-		startDegrees: 45.704148742596445,
-		color: '#eac580'
-	},
-	{
-		name: 'Uranus',
-		diameter: 51118,
-		period: 30589.0,
-		distance: 2867.0,
-		startDegrees: 316.4132007782158,
-		color: '#85bdbe'
+		name: 'Pluto',
+		diameter: 2376,
+		period: 90560,
+		distance: 5906.4,
+		startDegrees: 250.54296438557793,
+		color: '#b4aa9a'
 	},
 	{
 		name: 'Neptune',
@@ -64,11 +16,59 @@ export default [
 		color: '#6a88bb'
 	},
 	{
-		name: 'Pluto',
-		diameter: 2376,
-		period: 90560,
-		distance: 5906.4,
-		startDegrees: 250.54296438557793,
-		color: '#b4aa9a'
+		name: 'Uranus',
+		diameter: 51118,
+		period: 30589.0,
+		distance: 2867.0,
+		startDegrees: 316.4132007782158,
+		color: '#85bdbe'
+	},
+	{
+		name: 'Saturn',
+		diameter: 120536,
+		period: 10747.0,
+		distance: 1432.0,
+		startDegrees: 45.704148742596445,
+		color: '#eac580'
+	},
+	{
+		name: 'Jupiter',
+		diameter: 142984,
+		period: 4332.0,
+		distance: 778.5,
+		startDegrees: 36.24901810964624,
+		color: '#bb9b87'
+	},
+	{
+		name: 'Mars',
+		diameter: 6792,
+		period: 687.0,
+		distance: 227.9,
+		startDegrees: 359.13427034704614,
+		color: '#f27b5f'
+	},
+	{
+		name: 'Earth',
+		diameter: 12756,
+		period: 365.2,
+		distance: 149.6,
+		startDegrees: 99.86813733232074,
+		color: '#5e98b2'
+	},
+	{
+		name: 'Venus',
+		diameter: 12104,
+		period: 224.7,
+		distance: 108.2,
+		startDegrees: 181.79362286556446,
+		color: '#d79a4c'
+	},
+	{
+		name: 'Mercury',
+		diameter: 4879,
+		period: 88.0,
+		distance: 57.9,
+		startDegrees: 252.4100389327003,
+		color: '#bfbdbc'
 	}
 ];
