32a578b84048d84a25b6d1b68ce1de2fa9e2bf1e
refact0r <yifanw06@gmail.com>
2024-09-08 11:22:31 -0700

    styling

--- src/lib/js/messages.js
+++ src/lib/js/messages.js
@@ -2,6 +2,7 @@ export function filterMessages(messages, profile, messagesRead, n = 0) {
 	let newMessages = messages
 		.filter((message) => message.to === profile)
 		.filter((message) => !messagesRead.includes(message.id))
+		.filter((message) => new Date(message.arrives_at) < new Date())
 		.sort((a, b) => b.created_at - a.created_at)
 		.map((message) => {
 			return {
@@ -12,6 +13,7 @@ export function filterMessages(messages, profile, messagesRead, n = 0) {
 	let oldMessages = messages
 		.filter((message) => message.to !== profile)
 		.filter((message) => messagesRead.includes(message.id))
+		.filter((message) => new Date(message.arrives_at) < new Date())
 		.sort((a, b) => b.created_at - a.created_at)
 
 		.map((message) => {
