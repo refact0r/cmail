dcd9c74904557252a2ee41aea07c36f9fe63761e
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-08 11:51:56 -0700

    inbox

--- src/routes/inbox/[slug]/+page.svelte
+++ src/routes/inbox/[slug]/+page.svelte
@@ -1,8 +1,28 @@
 <script>
+	import { formatDate, formatTime } from '$lib/js/utils.js';
+
 	export let data;
+
+	$: message = data.messages.find((msg) => String(msg.id) === String(data.id));
 </script>
 
-<div class="container box"></div>
+<div class="container box">
+	<h2>message</h2>
+	<p>from: <code>{message.from}</code></p>
+	<p>to: <code>{message.to}</code></p>
+	<p>sent: <code>{formatDate(message.created_at)} {formatTime(message.created_at)}</code></p>
+	<p>arrives: <code>{formatDate(message.arrives_at)} {formatTime(message.arrives_at)}</code></p>
+	<br />
+	<p>
+		{message.content}
+	</p>
+</div>
 
 <style>
+	h2 {
+		margin-top: 0;
+	}
+	.container {
+		flex: 1;
+	}
 </style>
