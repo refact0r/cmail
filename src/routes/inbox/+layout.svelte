226fabb581d6931bd6c7e5de96de6d287ede59f4
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-08 11:22:31 -0700

    styling

--- src/routes/inbox/+layout.svelte
+++ src/routes/inbox/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { profile } from '$lib/stores/profile.js';
 	import { messagesRead } from '$lib/stores/messagesRead.js';
+	import { filterMessages } from '$lib/js/messages.js';
+	import { formatDate, formatTime } from '$lib/js/utils.js';
 
 	export let data;
 
-	$: messages = filteredMessages(data.messages, $profile, $messagesRead);
+	$: messages = filterMessages(data.messages, $profile, $messagesRead);
 
 	let select = 'incoming';
 </script>
@@ -25,12 +27,28 @@
 				on:click={() => (select = 'outgoing')}>Outgoing</button
 			>
 		</div> -->
-		<div class="messages">
-			{#each messages as message}
-				<a class="message" href="/inbox/{message.id}"></a>
-			{/each}
+		<div class="side box">
+			<div class="messages">
+				{#each messages as message}
+					<a class="message box2" href="/inbox/{message.id}">
+						<div class="content-container">
+							<p class="content">{message.content}</p>
+							<div class="new">•</div>
+						</div>
+						<p class="details">
+							<span class="from">{message.from}</span>
+							<span class="separator">•</span>
+							<span class="date">{formatDate(message.created_at)}</span>
+							<span class="separator">•</span>
+							<span class="time">{formatTime(message.created_at)}</span>
+						</p>
+					</a>
+				{/each}
+			</div>
 		</div>
-		<slot />
+		<div class="side">
+			<slot />
+		</div>
 	</div>
 </div>
 
@@ -40,57 +58,15 @@
 		text-align: center;
 	}
 	.container {
-		padding: 0rem 4rem;
 		display: flex;
-		gap: 1rem;
+		gap: 2rem;
 	}
-	.setting {
-		flex: 15%;
-		border-radius: 1rem;
-		padding: 0rem 1rem;
+	.side {
+		width: 50%;
 	}
 	.messages {
-		flex: 85%;
-		min-height: 50vh;
-		background-color: var(--bg-3);
-		border-radius: 1rem;
-		padding: 1rem;
-	}
-	button {
-		font-family: 'Dm Sans', sans-serif;
-		font-size: 1rem;
-		background: var(--bg);
-		color: var(--fg);
-		text-decoration: none;
-		text-align: left;
-		padding: 1rem 0.5rem;
-		width: 100%;
-		transition: 0.3s;
-	}
-	button:hover {
-		background: var(--bg-2);
-		cursor: pointer;
-	}
-	#incoming {
-		border-top: 0.2rem solid var(--bg-3);
-		border-left: 0.2rem solid var(--bg-3);
-		border-right: 0.2rem solid var(--bg-3);
-		border-bottom: none;
-		border-radius: 1rem 1rem 0rem 0rem;
-	}
-	#outgoing {
-		border-bottom: 0.2rem solid var(--bg-3);
-		border-left: 0.2rem solid var(--bg-3);
-		border-right: 0.2rem solid var(--bg-3);
-		border-top: none;
-		border-radius: 0rem 0rem 1rem 1rem;
-	}
-	.selected {
-		background-color: var(--bg-3);
-	}
-	.message {
-		background-color: hsla(0, 0%, 100%, 0.2);
-		border-radius: 1rem;
-		padding: 0.5rem 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
 	}
 </style>
